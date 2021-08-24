cd7db843c5b4ce908122c1760f598e06d2380f80
masaya-kawamura <lucky.number0128@gmail.com>
2021-08-25 07:15:28 +0900

    [add]production error pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
   layout :set_layout
 
-  private 
-  def set_layout
+  class Forbidden < ActionController::ActionControllerError; end
+  class IpAddressRejected < ActionController::ActionControllerError; end
+
+  include ErrorHandlers if Rails.env.prodction?
+
+  private def set_layout
     if params[:controller].match(%r{\A(staff|admin|customer)/})
       Regexp.last_match[1]
     else
