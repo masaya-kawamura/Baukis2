226dab810aa6d038e42e250e2d7d7673192f460f
masaya-kawamura <lucky.number0128@gmail.com>
2021-08-26 13:17:40 +0900

    [add]staff & admin sessions

--- app/models/administrator.rb
+++ app/models/administrator.rb
@@ -1,4 +1,4 @@
-class StaffMember < ApplicationRecord
+class Administrator < ApplicationRecord
   def password=(raw_password)
     if raw_password.kind_of?(String)
       self.hashed_password = BCrypt::Password.create(raw_password)
